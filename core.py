d6b01554e8413a7aef9bbf2d101baccee623b032
Ahmad Elawady <ai.elawady@gmail.com>
2024-06-08 11:04:16 +0300

    Fix error

--- core.py
+++ core.py
@@ -2,5 +2,8 @@
 from typing import Union
 
 def sum2nums(a: Union[float, int], b: Union[float, int]) -> float:
-    """Sums 2 numbers."""
-    return a + b + 1
+    """Sums 2 numbers.
+
+    returns a + b.
+    """
+    return a + b
